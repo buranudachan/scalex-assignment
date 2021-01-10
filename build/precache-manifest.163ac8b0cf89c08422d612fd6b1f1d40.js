2fe8f031ec9b8bda07b3ef45aa51e99462e026e7
Burhan <buran.udachan@rightbraininfotech.in>
2021-01-10 11:02:25 +0530

    di file added

---
+++ build/precache-manifest.163ac8b0cf89c08422d612fd6b1f1d40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc6d6741031e1b52abf6a2fec1888699",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f23d466371abc4c1f26e",
+    "url": "/static/css/main.fdf3b9c3.chunk.css"
+  },
+  {
+    "revision": "fb30c74d305243f565c6",
+    "url": "/static/js/2.a59a54a5.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.a59a54a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f23d466371abc4c1f26e",
+    "url": "/static/js/main.2aa4038e.chunk.js"
+  },
+  {
+    "revision": "8e4567f80205840d841f",
+    "url": "/static/js/runtime-main.7ef55be1.js"
+  },
+  {
+    "revision": "b35050eb1e650076706efc9f62409471",
+    "url": "/static/media/sample.b35050eb.png"
+  }
+]);
